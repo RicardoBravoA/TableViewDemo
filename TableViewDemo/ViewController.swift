3bc14fdee590f5b0bb9446dba1cb71fa6d4376e5
Ricardo Bravo <ricardobravo@outlook.com>
2016-06-11 13:54:14 -0500

    chore(init): initial commit

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var data = ["Item 1", "Item 2", "Item 3", "Item 4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
